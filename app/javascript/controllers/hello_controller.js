8acef4b50d9567310523636ea5a64f08c898bd2e
racar <rcarrascal@gmail.com>
2020-09-29 00:07:13 -0500

    base styling and stimulus test

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from 'stimulus';
 export default class extends Controller {
+    static targets = ["heading"]
     connect() {
         console.log("hello from StimulusJS")
+    }
+
+    greet() {
+        console.log("click");
+        this.headingTarget.innerHTML = "Hello World";
     }
 }
