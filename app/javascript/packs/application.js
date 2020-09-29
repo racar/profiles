4a54e05a0cb1f2a12dd07766c0e2327af55acf93
racar <rcarrascal@gmail.com>
2020-09-28 22:25:47 -0500

    adding user model and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,9 +16,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import 'bootstrap';
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+import 'controllers';
