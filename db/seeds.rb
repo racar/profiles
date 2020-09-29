bab6085d9afbcfdd285c1927fba9db2cf65a404c
racar <rcarrascal@gmail.com>
2020-09-29 00:57:02 -0500

    add user sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Loading data from: https://randomuser.me/documentation#howto
+
+
+load_users = JSON.parse(RestClient.get('https://randomuser.me/api/?results=5'))['results']
+
+load_users.each do |load_user|
+  u = User.new
+  u.name = load_user["name"]["title"] + " " + load_user["name"]["first"] + " " + load_user["name"]["last"]
+  u.email = load_user["email"]
+  u.country = load_user["location"]["country"]
+  u.photo = load_user["picture"]["large"]
+  u.save
+end
